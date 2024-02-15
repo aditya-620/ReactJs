bf83db2b5eed318c74da9bcb0160dcd19c1b9310
Aditya <adityarastogi620@gmail.com>
2024-02-16 03:08:42 +0530

    basic setup - vite react

---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,13 @@
+import Test from './Test.jsx'
+
+function App() {
+
+  return (
+    <>
+    <h1>Aditya Rastogi</h1>
+    <Test />
+    </>
+  )
+}
+
+export default App
---
+++ 01_vite_react/src/Test.jsx
@@ -0,0 +1,7 @@
+function Test(){
+    return(
+        <h2>this is test</h2>
+    )
+}
+
+export default Test
