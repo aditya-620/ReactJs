acfde1fef9e7006d450ef5a31a2d55f75ee4a294
Aditya <adityarastogi620@gmail.com>
2024-02-18 02:41:27 +0530

    custom react element

--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.jsx'
 
 
+//as we know in app.jsx there is app method, so we created our on
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    )
+}
+
+
+//at the end the react also make the method to tree like structure
+//so, we also created our own object, but it didn't work
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+
+const anotherUser = "chai aur react"
+
+
+//this uses babble(transcripter), and automatically injects, and follows same sets of way to write
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank' },
+    'click me to visit google',
+    anotherElement
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//  MyApp
+    reactElement
+  
 )
---
+++ 02_customReact/customreact.js
@@ -0,0 +1,34 @@
+//here we are creating our own react element and then rendring it
+
+function customRender(reactElement, container){
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href', reactElement.props.href)
+    domElement.setAttribute('target', reactElement.props.target)
+
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+}
+
+//here, we have created our own element, and also expecting that you should give the same pattern of the element as below
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
