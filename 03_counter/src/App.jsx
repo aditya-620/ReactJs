6afd0806d068319535b28ea6196ad180bd8efa16
Aditya <adityarastogi620@gmail.com>
2024-02-28 01:46:28 +0530

    interview ques

--- 03_counter/src/App.jsx
+++ 03_counter/src/App.jsx
@@ -13,8 +13,26 @@ function App() {
 
   const addValue = () => {
     if(counter <20 && counter >= 0){
-      counter = counter + 1
-      setCounter(counter)
+      // counter = counter + 1
+      // setCounter(counter)
+
+      //interview ques: what if we have to do setcounter(counter + 1) 4 times
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      // setCounter(counter + 1)
+      //do will it increment the value from 15 to 19 in onw click?
+      //NO, as the useState created a whole package, it see that all the method is doing the same thing so the counter will increment from 1 only (i.e. 15 to 16)
+      
+      //so, isin't there any way?
+      //the setCounter method have a call back
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter((prevCounter) => prevCounter + 1)
+      setCounter(prevCounter => prevCounter + 1)
+      //now, they do not go as a bunch of package and beacuse of it when line 28 return then line 29 execute and so on
+
+      //here: prevCounter, counter, setCounter are just names and can be of any name what the programmer want
     }
   }
 
