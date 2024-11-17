accf3fe4c89ecd0052634e78d1adba78195afff4
Aditya <adityarastogi620@gmail.com>
2024-11-17 22:35:52 +0530

    currency convertor

--- 07_currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 07_currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -2,9 +2,9 @@ import {useEffect, useState} from "react"
 
 
 function useCurrencyInfo(currency){
-    const [data, setData] = useState({})
+    const [data, setData] = useState({})  //here {} are used beacuse, if fetch didn't work then the website will not crash as we can run the loop on the object
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
         console.log(data);
